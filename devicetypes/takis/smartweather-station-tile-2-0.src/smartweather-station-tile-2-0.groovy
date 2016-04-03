be64f83da6aeedccfc671a9e9fef75947c89ca60
John LoVerso <john.loverso@gmail.com>
2016-04-03 01:16:44 -0400

    Renamed to enable IDE integration with Github

--- devicetypes/takis/smartweather-station-tile-2-0.src/smartweather-station-tile-2-0.groovy
+++ devicetypes/takis/smartweather-station-tile-2-0.src/smartweather-station-tile-2-0.groovy
@@ -17,7 +17,7 @@
  *  Date: 2013-04-30
  */
 metadata {
-	definition (name: "SmartWeather Station Tile 2.0", namespace: "smartthings/miro", author: "Takis") {
+	definition (name: "SmartWeather Station Tile 2.0", namespace: "takis", author: "Takis") {
 		capability "Illuminance Measurement"
 		capability "Temperature Measurement"
 		capability "Relative Humidity Measurement"
